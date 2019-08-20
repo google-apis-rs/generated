2dcc5b2b0abe468b9900dd8356cb7566cd420b49
Sebastian Thiel <sthiel@thoughtworks.com>
2019-08-20 14:06:29 +0800

    On CI, check everything, too!
    Because we can :D

--- ci/script.sh
+++ ci/script.sh
@@ -3,7 +3,7 @@
 set -eu
 
 make update-all-metadata generate-makefile
-make -C gen gen-all -k || {
+make -C gen gen-all cargo-all ARGS=check -k || {
     res=$?
     make show-all-errors
     exit $res
